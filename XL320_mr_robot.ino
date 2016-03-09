c8a79ca4d1fad90bf3b63cef388e8612da91d667
sighmon <simon.loffler@gmail.com>
2016-03-09 20:47:50 +1030

    Added standing pose, and hip wiggle test movement.

--- XL320_mr_robot.ino
+++ XL320_mr_robot.ino
@@ -1,4 +1,5 @@
 
+
 // ======================================
 // Dynamixel XL-320 three servo robot arm
 // ======================================
@@ -29,16 +30,37 @@ int delayBetweenSendReceive = 10;
 int servoOneID = 1;
 int servoTwoID = 2;
 int servoThreeID = 3;
+int servoFourID = 4;
+int servoFiveID = 5;
+int servoSixID = 6;
+int servoSevenID = 7;
+int servoEightID = 8;
+int servoNineID = 9;
 
 // Set the servo LEDs
 int servoOneLED = 0;
 int servoTwoLED = 0;
 int servoThreeLED = 0;
+int servoFourLED = 0;
+int servoFiveLED = 0;
+int servoSixLED = 0;
+int servoSevenLED = 0;
+int servoEightLED = 0;
+int servoNineLED = 0;
 
-// Remember the last servo position to change the LED colour when moving
-int servoOnePosition = 0;
-int servoTwoPosition = 0;
-int servoThreePosition = 0;
+// Zero starting positions
+int servoOnePosition = 500;
+int servoTwoPosition = 500;
+int servoThreePosition = 450;
+int servoFourPosition = 500;
+int servoFivePosition = 500;
+int servoSixPosition = 500;
+int servoSevenPosition = 500;
+int servoEightPosition = 500;
+int servoNinePosition = 500;
+
+int positionStore = 500;
+int goingUp = true;
 
 byte buffer[256];
 
@@ -60,65 +82,150 @@ void setup() {
   pinMode(laserPin, OUTPUT);
   
   // Talk serial with your servo by connecting the servo input data line to Digital TX 1
-  HalfDuplexSerial.begin(115200);
-  HalfDuplexSerial.setTimeout(delayBetweenSendReceive);
+  // Use HalfDuplexSerial1 for a Leonardo, or HalfDuplexSerial for a Uno.
+  HalfDuplexSerial1.begin(115200);
+  HalfDuplexSerial1.setTimeout(delayBetweenSendReceive);
 
   // Initialise your robot
-  robot.begin(HalfDuplexSerial); // Hand in the serial object you're using
+  robot.begin(HalfDuplexSerial1); // Hand in the serial object you're using
   
   // I like fast moving servos, so set the joint speed to max!
-  robot.setJointSpeed(servoOneID, 1023);
-  robot.readPacket(buffer,256); // After each command to be ready for next command
-  robot.setJointSpeed(servoTwoID, 1023);
-  robot.readPacket(buffer,256);
-  robot.setJointSpeed(servoThreeID, 1023);
-  robot.readPacket(buffer,256);
+  for (int i = 1; i < 10; i++) {
+    robot.setJointSpeed(i, 1023);
+    robot.readPacket(buffer,256);
+  }
+
+  // Squat position
+  servoOnePosition = 500;
+  servoTwoPosition = 350;
+  servoThreePosition = 550;
+  servoFourPosition = 500;
+  servoFivePosition = 350;
+  servoSixPosition = 650;
+  servoSevenPosition = 500;
+  servoEightPosition = 350;
+  servoNinePosition = 650;
 
 }
 
 void loop() {
   
-  servoOneLED = servoTwoLED = servoThreeLED = stillColour;
-
-  int servoOnePositionNow = 1024 - analogRead(A0);
-  int servoTwoPositionNow = analogRead(A1);
-  int servoThreePositionNow = analogRead(A2);
-
-  // Set LED to red when moving
-  if (abs(servoOnePosition - servoOnePositionNow) > 10) {
-    servoOneLED = movingColour;
-  }
-  if (abs(servoTwoPosition - servoTwoPositionNow) > 10) {
-    servoTwoLED = movingColour;
-  }
-  if (abs(servoThreePosition - servoThreePositionNow) > 10) {
-    servoThreeLED = movingColour;
+  // ******* MOVING TWO JOINTS ON EACH LEG ******** //
+//  int positionNow = positionStore;
+//  if (goingUp && positionStore < 550) {
+//    positionNow += 10;
+//  } else {
+//    goingUp = false;
+//  }
+//  if (!goingUp && positionStore > 450) {
+//    positionNow -= 10;
+//  } else {
+//    goingUp = true;
+//  }
+//  positionStore = positionNow;
+//  robot.moveJoint(2, positionStore);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(3, positionStore);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(5, positionStore);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(6, positionStore);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(8, positionStore);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(9, positionStore);
+//  robot.readPacket(buffer,256);
+  // ******* END MOVING TWO JOINTS ON EACH LEG ******** //
+  
+  // ******* START SETTINGS ******* //
+  // Squat positions
+//  servoOnePosition = 500;
+//  servoTwoPosition = 350;
+//  servoThreePosition = 550;
+//  servoFourPosition = 500;
+//  servoFivePosition = 350;
+//  servoSixPosition = 650;
+//  servoSevenPosition = 500;
+//  servoEightPosition = 350;
+//  servoNinePosition = 650;
+//  robot.moveJoint(1, servoOnePosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(2, servoTwoPosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(3, servoThreePosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(4, servoFourPosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(5, servoFivePosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(6, servoSixPosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(7, servoSevenPosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(8, servoEightPosition);
+//  robot.readPacket(buffer,256);
+//  robot.moveJoint(9, servoNinePosition);
+//  robot.readPacket(buffer,256);
+  
+  int servoOnePositionNow = servoOnePosition;
+  int servoFourPositionNow = servoFourPosition;
+  int servoSevenPositionNow = servoSevenPosition;
+  
+  if (goingUp && servoOnePosition < 600) {
+    servoOnePositionNow += 10;
+    servoFourPositionNow += 10;
+    servoSevenPositionNow += 10;
+  } else {
+    goingUp = false;
   }
   
-  // Fade the laser intensity
-  analogWrite(laserPin, laserIntensity);
-  laserIntensity = (laserIntensity + 1) % 10;
-
-  // Set servoPositions
+  if (!goingUp && servoOnePosition > 400) {
+    servoOnePositionNow -= 10;
+    servoFourPositionNow -= 10;
+    servoSevenPositionNow -= 10;
+  } else {
+    goingUp = true;
+  }
+//
+//  // Set LED to red when moving
+//  if (abs(servoOnePosition - servoOnePositionNow) > 10) {
+//    servoOneLED = movingColour;
+//  }
+//  if (abs(servoFourPosition - servoFourPositionNow) > 10) {
+//    servoTwoLED = movingColour;
+//  }
+//  if (abs(servoSevenPosition - servoSevenPositionNow) > 10) {
+//    servoThreeLED = movingColour;
+//  }
+//  
+//  // Fade the laser intensity
+//  analogWrite(laserPin, laserIntensity);
+//  laserIntensity = (laserIntensity + 10) % 1024;
+//
+//  // Set servoPositions
   servoOnePosition = servoOnePositionNow;
-  servoTwoPosition = servoTwoPositionNow;
-  servoThreePosition = servoThreePositionNow;
+  servoFourPosition = servoFourPositionNow;
+  servoSevenPosition = servoSevenPositionNow;
   
   // Set LED Red
-  robot.LED(servoOneID, &rgb[servoOneLED] );
-  robot.readPacket(buffer,256);
-  robot.LED(servoTwoID, &rgb[servoTwoLED] );
-  robot.readPacket(buffer,256);
-  robot.LED(servoThreeID, &rgb[servoThreeLED] );
-  robot.readPacket(buffer,256);
+  for (int i = 1; i < 10; i++) {
+    robot.LED(i, &rgb[random(1,7)] );
+    robot.readPacket(buffer,256);
+  }
 
-  // Servo test.. select a random servoID and colour
+  // Servo test..
   robot.moveJoint(servoOneID, servoOnePosition);
   robot.readPacket(buffer,256);
-  robot.moveJoint(servoTwoID, servoTwoPosition);
+  robot.moveJoint(servoFourID, servoFourPosition);
   robot.readPacket(buffer,256);
-  robot.moveJoint(servoThreeID, servoThreePosition);
+  robot.moveJoint(servoSevenID, servoSevenPosition);
   robot.readPacket(buffer,256);
+
+  // Random moving code.
+//  for (int i = 1; i < 10; i++) {
+//    robot.moveJoint(i, random(450, 550));
+//    robot.readPacket(buffer,256);
+//  }
   
   // To read back from the servo
 //  XL320::Packet pp = XL320::Packet(buffer, robot.readPacket(buffer,256));
